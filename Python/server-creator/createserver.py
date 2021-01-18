63d3cd3c3bc6378b7c183a55ad0cd3de52e997c8
LeNinjaHD <cedric@leprofi.com>
2021-01-18 11:38:20 +0100

    Addes Support for Every Server Version

--- Python/server-creator/createserver.py
+++ Python/server-creator/createserver.py
@@ -20,24 +20,125 @@ clear()
 print("Create New Server")
 line(2)
 name = input("How should the Server be called? \n> ")
-type = input("What Servertype should the Server be? (Spigot) \n> ")
-if(type == "Spigot"):
-    version = input("Which Version should the Spigot Server be? (1.16.4) \n> ")
-    if(version == "1.16.4"):
+type = input("What Servertype should the Server be? (Paper) \n> ")
+if(type == "Paper"):
+    version = input("Which Version should the Spigot Server be? (1.16.5, 1.15.2, 1.8.8) \n> ")
+    if(version == "1.16.5"):
         port = input("Which Port should the Server be? \n> ")
         line(1)
         os.mkdir(name)
-        startscript = open(name + "/start.sh", "w")
-        startscript.write("java -jar spigot.jar")
-        startscript.close()
-        eula = open(name + "/eula.txt", "w")
-        eula.write("eula=true")
-        eula.close()
-        sp = open(name + "/server.properties", "w")
-        sp.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
-        sp.close
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
         os.chmod(name + "/start.sh", 0o777)
-        urllib.request.urlretrieve("https://leprofi.com/download/servermanager/spigot-1.16.4.jar", name + "/spigot.jar")
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/429/downloads/paper-1.16.5-429.jar", name + "/paper.jar")
+        print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    elif(version == "1.15.2"):
+        port = input("Which Port should the Server be? \n> ")
+        line(1)
+        os.mkdir(name)
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
+        os.chmod(name + "/start.sh", 0o777)
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.15.2/builds/391/downloads/paper-1.15.2-391.jar", name + "/paper.jar")
+        print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    elif(version == "1.14.4"):
+        port = input("Which Port should the Server be? \n> ")
+        line(1)
+        os.mkdir(name)
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
+        os.chmod(name + "/start.sh", 0o777)
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.14.4/builds/243/downloads/paper-1.14.4-243.jar", name + "/paper.jar")
+        print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    elif(version == "1.13.2"):
+        port = input("Which Port should the Server be? \n> ")
+        line(1)
+        os.mkdir(name)
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
+        os.chmod(name + "/start.sh", 0o777)
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.13.2/builds/655/downloads/paper-1.13.2-655.jar", name + "/paper.jar")
+        print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    elif(version == "1.12.2"):
+        port = input("Which Port should the Server be? \n> ")
+        line(1)
+        os.mkdir(name)
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
+        os.chmod(name + "/start.sh", 0o777)
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.12.2/builds/1618/downloads/paper-1.12.2-1618.jar", name + "/paper.jar")
+        print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    elif(version == "1.11.2"):
+        port = input("Which Port should the Server be? \n> ")
+        line(1)
+        os.mkdir(name)
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
+        os.chmod(name + "/start.sh", 0o777)
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.11.2/builds/1104/downloads/paper-1.11.2-1104.jar", name + "/paper.jar")
+        print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    elif(version == "1.10.2"):
+        port = input("Which Port should the Server be? \n> ")
+        line(1)
+        os.mkdir(name)
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
+        os.chmod(name + "/start.sh", 0o777)
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.10.2/builds/916/downloads/paper-1.10.2-916.jar", name + "/paper.jar")
+        print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    elif(version == "1.9.4"):
+        port = input("Which Port should the Server be? \n> ")
+        line(1)
+        os.mkdir(name)
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
+        os.chmod(name + "/start.sh", 0o777)
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.9.4/builds/773/downloads/paper-1.9.4-773.jar", name + "/paper.jar")
+        print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    elif(version == "1.8.8"):
+        port = input("Which Port should the Server be? \n> ")
+        line(1)
+        os.mkdir(name)
+        with open(name + "/start.sh", "w") as f:
+            f.write("java -jar paper.jar nogui")
+        with open(name + "/eula.txt", "w") as f:  
+            f.write("eula=true")
+        with open(name + "/server.properties", "w") as f:
+            f.write("motd=Made with ServerManager by LeNinjaHD and Astrago\nserver-port=" + port)
+        os.chmod(name + "/start.sh", 0o777)
+        urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/443/downloads/paper-1.8.8-443.jar", name + "/paper.jar")
         print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
 else:
     print("Servertype not found.")
