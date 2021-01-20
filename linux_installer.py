c95c3210a195df05562589b2adccf92c29848653
LeNinjaHD <cedric@leprofi.com>
2021-01-20 15:10:47 +0100

    Linux Installer now gets Directory automatically

--- linux_installer.py
+++ linux_installer.py
@@ -1,7 +1,9 @@
+import pathlib
 print("ServerManager Linux Installer")
 print("")
 print("")
-path = input("Where is the ServerManager Installation located? (for example: /home/user/servermanager) \n> ")
+#path = input("Where is the ServerManager Installation located? (for example: /home/user/servermanager) \n> ")
+pathlib.Path(__file__).parent.absolute()
 with open("/usr/bin/servermanager", "w") as f:
     f.write("#!/bin/bash\n")
     f.write("python3 " + path + "servermanager.py")
