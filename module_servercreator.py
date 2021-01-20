b32565e5792613b5eacfd3ef79ff658f6f6cc130
LeNinjaHD <cedric@leprofi.com>
2021-01-20 20:03:05 +0100

    Updated Module

--- module_servercreator.py
+++ module_servercreator.py
@@ -140,6 +140,8 @@ if(type == "Paper"):
         os.chmod(name + "/start.sh", 0o777)
         urllib.request.urlretrieve("https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/443/downloads/paper-1.8.8-443.jar", name + "/paper.jar")
         print("Your New Server is in " + name + "/. Enter ./start.sh to start the Server. It will be available on Port " + port + ".")
+    else:
+        print("Version Not Found")
 elif(type == "Vanilla"):
     version = input("Which Version should the Vanilla Server be? (1.16.5, 1.15.2, 1.14.4, 1.13.2, 1.12.2, 1.11.2, 1.10.2, 1.9.4, 1.8.8) \n> ")
     if(version == "1.16.5"):
